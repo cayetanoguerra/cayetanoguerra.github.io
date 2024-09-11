9fb431d58a1b9e7302a6dd884278890f2f46ecfa
Cayetano Guerra Artal <cayetanoguerra@gmail.com>
2024-09-11 15:00:01 +0100

    AP

--- Adevinta-NLP/OpenAI/bot.py
+++ Adevinta-NLP/OpenAI/bot.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 class Bot:
     def __init__(self):
-        self.client = OpenAI(api_key="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")
+        self.client = OpenAI(api_key="")
 
     def chat(self, messages):
         response = self.client.chat.completions.create(
